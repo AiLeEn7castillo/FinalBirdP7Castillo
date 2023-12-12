3fa950a4f627fee9f067f7e4ff65b0ef3f7220ba
AiLeEn7castillo <aileen07castillo@gmail.com>
2023-12-12 15:21:17 -0800

    Adding Columns
    I have added the columns to my scene.

--- FlappyFinalP7Castillo/Assets/Column.cs
+++ FlappyFinalP7Castillo/Assets/Column.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameCControll : MonoBehaviour
+public class Column : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
